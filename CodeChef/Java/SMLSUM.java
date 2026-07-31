ec6a843b8e9d7d5722921d00111f17d65000ca03
Priyanshu . <priyanshujangra145@gmail.com>
2026-07-31 13:41:29 +0530

    Add solution for SMLSUM (Java) [CodeSync]

--- CodeChef/Java/SMLSUM.java
+++ CodeChef/Java/SMLSUM.java
@@ -1,14 +1,3 @@
-            Pair[] pairs = new Pair[n];
-            String[] aStr = br.readLine().split(" ");
-            String[] bStr = br.readLine().split(" ");
-            for (int i = 0; i < n; i++) {
-                pairs[i] = new Pair(Integer.parseInt(aStr[i]
-                    ), Integer.parseInt(bStr[i]));
-            }
-            Arrays.sort(pairs);
-
-            long[] prefixSum = new long[n + 1];
-            for (int i = 1; i <= n; i++) {
                 prefixSum[i] = prefixSum[i - 1] + pairs[i - 
                     1].b;
             }
@@ -29,3 +18,11 @@
         while (low < high) {
             int mid = low + (high - low) / 2;
             if (arr[mid].a <= target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
