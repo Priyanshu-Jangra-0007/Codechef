84d2ab7f5530e6ef43a1610ec8bc59f30ee7e272
Priyanshu . <priyanshujangra145@gmail.com>
2026-07-31 13:31:45 +0530

    Add solution for BOMBTHEBASE (Java) [CodeSync]

---
+++ CodeChef/Java/BOMBTHEBASE.java
@@ -0,0 +1,28 @@
+{
+    public static void main (String[] args) throws java.lang
+        .Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt(); 
+
+        while (t-- > 0) {
+            int n = scanner.nextInt(); 
+            int x = scanner.nextInt(); 
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+
+            int ans = 0;
+            for (int i = n - 1; i >= 0; i--) {
+                if (a[i] < x) {
+                    ans = i + 1;
+                    break;
+                }
+            }
+            System.out.println(ans); 
+        }
+        scanner.close();
+    }
+}
