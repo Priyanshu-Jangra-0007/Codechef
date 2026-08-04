95e9a57d7fad40127b5838a594e16ed785e723da
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-04 14:24:02 +0530

    Add solution for MAJORELEM (Java) [CodeSync]

---
+++ CodeChef/Java/MAJORELEM.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] arr) {
+        int candidate = arr[0];
+        int count = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = arr[i];
+                    count = 1;
+                }
+            }
+        }
+        return candidate; // Guaranteed to exist
+    }
+}
