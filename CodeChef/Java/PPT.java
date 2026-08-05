13c121a8c87df9a961cecf76302d241a973a50d3
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-05 20:19:36 +0530

    Add solution for PPT (Java) [CodeSync]

---
+++ CodeChef/Java/PPT.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(20-n);
+    }
+}
+
+int local
+in snippet
+IncompatibleClassChangeError keyword
+IndexOutOfBoundsException keyword
+Infinity keyword
+info snippet
+InheritableThreadLocal keyword
+instanceof keyword
+InstantiationError keyword
