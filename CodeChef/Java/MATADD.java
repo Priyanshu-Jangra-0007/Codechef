b2853f05c32706f818f9bee046f7c89ddf4e5216
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-07 19:05:08 +0530

    Add solution for MATADD (Java) [CodeSync]

--- CodeChef/Java/MATADD.java
+++ CodeChef/Java/MATADD.java
@@ -1,31 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         int[][] a = new int[n][m];
         int[][] b = new int[n][m];
 
+        // Read matrix A
         for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                a[i][j] = scanner.nextInt();
+                a[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        // Read matrix B
         for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                b[i][j] = scanner.nextInt();
+                b[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(a[i][j] + b[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-}
