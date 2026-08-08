d20bb38b284e94689b7fd2201215e65ead929ae3
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-08 12:30:48 +0530

    Add solution for MATSORTDIAG (Java) [CodeSync]

---
+++ CodeChef/Java/MATSORTDIAG.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static int[][] diagonalSort(int[][] mat) {
+        int row = mat.length, col = mat[0].length;
+        HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                int key = i - j;
+                if (!d.containsKey(key)) {
+                    d.put(key, new PriorityQueue<>());
+                }
+                d.get(key).offer(mat[i][j]);
+            }
+        }
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                int key = i - j;
+                mat[i][j] = d.get(key).poll();
+            }
+        }
+        
+        return mat;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
