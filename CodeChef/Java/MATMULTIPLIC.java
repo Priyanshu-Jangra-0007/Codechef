a0f27a81255b0af5e9ce995e5ed5fa30f911c1cf
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-08 12:30:31 +0530

    Add solution for MATMULTIPLIC (Java) [CodeSync]

---
+++ CodeChef/Java/MATMULTIPLIC.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        int[][] a = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                a[i][j] = scanner.nextInt();
+            }
+        }
+
+        int _n = n;
+
+        n = scanner.nextInt();
+        int p = scanner.nextInt();
+
+        assert (_n == n);
+
+        int[][] b = new int[n][p];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < p; j++){
+                b[i][j] = scanner.nextInt();
+            }
+        }
+
+        int[][] mul = new int[m][p];
